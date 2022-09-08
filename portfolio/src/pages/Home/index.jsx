316f2b3f05166205dfdf0bf17a77b0e49b724249
Renan Silva <renansilvaytB@gmail.com>
2022-09-08 10:33:36 -0300

    feat home content

--- portfolio/src/pages/Home/index.jsx
+++ portfolio/src/pages/Home/index.jsx
@@ -1,11 +1,46 @@
 import { Container } from './style'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+import my_photo from '../../assets/my_photo.png'
 
 export function Home() {
   return (
     <Container>
       <Header />
-      <h1>hello world</h1>
+      <main>
+        <section className="home-content" id="home">
+          <div className="title-content">
+            <h1 className="title-name">RENAN SILVA</h1>
+            <h1 className="title-introduction">DEV FRONT END</h1>
+          </div>
+          <div className="photo-content">
+            <img src={my_photo} alt="Foto de Renan Silva" />
+          </div>
+        </section>
+
+        <section className="about-content" id="about">
+          <div className="content">
+            <div className="text-content">
+              <p>
+                Prazer sou Renan Leandro da Silva 👋🏼, mas pode me chamar de
+                renyzera tambem. Sou desenvolvedor frontend, de Rio do Sul (SC)
+                📍. Serumaninho mente aberta, fã de formula 1 e automobilismo 🏎,
+                vive a vida de própositos e sonhos, ambicioso e parceiro para
+                tomar umas 🍻.
+              </p>
+              <p>
+                Desenvolvo projetos utilizando React.js, styled-components com
+                JavaScript, que vai desde projetos com API’s integradas até
+                Landing Pages. Algumas aplicações web que fiz, você pode
+                conferir logo abaixo.
+              </p>
+              <p>Tenho vivência com HTML5, CSS3, JavaScript</p>
+            </div>
+            <div className="title-content">
+              <h1 className="title-introduction">sobre</h1>
+            </div>
+          </div>
+        </section>
+      </main>
     </Container>
   )
 }
--- portfolio/src/pages/Home/style.js
+++ portfolio/src/pages/Home/style.js
@@ -1,3 +1,105 @@
 import styled from 'styled-components'
 
-export const Container = styled.main``
+export const Container = styled.div`
+  margin-top: 60px;
+  > main {
+    display: grid;
+    justify-items: center;
+
+    section {
+      padding: 105px 35px;
+    }
+
+    .title-introduction {
+      font-family: 'IBM Plex Mono';
+      font-weight: 700;
+      font-size: 128px;
+      line-height: 101px;
+      text-transform: uppercase;
+      background: linear-gradient(180deg, #b629f8 0%, #800add 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      background-clip: text;
+      text-fill-color: transparent;
+    }
+
+    .home-content {
+      display: flex;
+      max-width: 1120px;
+      width: 100vw;
+      justify-content: space-between;
+      .title-content {
+        margin: 30px 0 0 22px;
+        position: relative;
+
+        .title-name {
+          font-family: ${({ theme }) => theme.FONTS.FONT_IBM};
+          font-weight: 700;
+          font-size: 94px;
+          line-height: 80px;
+          text-transform: uppercase;
+          max-width: 200px;
+          color: #141414;
+
+          &::before {
+            content: '';
+            position: absolute;
+            left: -32px;
+            top: 8px;
+            width: 82px;
+            height: 12px;
+            background: ${({ theme }) => theme.COLORS.PURPLE_200};
+            transform: matrix(0.71, -0.59, 0.85, 0.71, 0, 0);
+            z-index: -1;
+          }
+          &::after {
+            content: '';
+            position: absolute;
+            left: -16px;
+            top: 10px;
+            width: 82px;
+            height: 11px;
+            background: ${({ theme }) => theme.COLORS.PURPLE_100};
+            transform: matrix(0.71, -0.59, 0.85, 0.71, 0, 0);
+            z-index: -1;
+          }
+        }
+
+        .title-introduction {
+          margin-top: 82px;
+          max-width: 455px;
+          text-align: right;
+        }
+      }
+
+      .photo-content img {
+        object-fit: cover;
+        object-position: center;
+        height: 100%;
+        padding-left: 24px;
+      }
+    }
+
+    .about-content {
+      background: #141414;
+      width: calc(100vw - 20px);
+
+      .content {
+        padding: 0 80px;
+        max-width: 1120px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        .text-content {
+          max-width: 75ch;
+        }
+
+        .title-content {
+          max-width: 25ch;
+          word-break: break-all;
+          text-align: right;
+        }
+      }
+    }
+  }
+`
