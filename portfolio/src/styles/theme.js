2a8a7804ab73bbc585ca6edfdf7f716656d12dd8
Renan Silva <renansilvaytB@gmail.com>
2022-09-08 08:41:48 -0300

    feat theme colors and fonts

---
+++ portfolio/src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    BACKGROUND_100: '#DADADA',
+    BACKGROUND_700: '#292929',
+    BACKGROUND_800: '#141414',
+
+    BLACK: '#000000',
+    PURPLE_100: '#A839FF',
+    PURPLE_200: '#701CDA',
+    WHITE: '#FFF',
+
+    GRAY_100: '#F5F5F5',
+    GRAY_300: '#666360',
+
+    // REGULAR & BOLD
+    F0NT_IBM: "'IBM Plex Mono', monospace;",
+    // REGULAR & BOLD
+    FONT_FIRA: "'Fira Sans', sans-serif;"
+  }
+}
