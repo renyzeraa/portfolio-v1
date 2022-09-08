deb842e4e2f13cc6270b1e7c74f217e7f11c9125
Renan Silva <renansilvaytB@gmail.com>
2022-09-08 08:47:14 -0300

    import global style

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
+import GlobalStyle from './styles/global'
+
 import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Home />
     </ThemeProvider>
   </React.StrictMode>
