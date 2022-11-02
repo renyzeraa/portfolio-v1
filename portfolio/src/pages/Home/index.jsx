3448049ce66661654dfe300cb3795df24e33934e
Renan Silva <renansilvaytB@gmail.com>
2022-11-02 14:57:41 -0300

    feat Home desktop

--- portfolio/src/pages/Home/index.jsx
+++ portfolio/src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
 import { Container } from './style'
 import { Header } from '../../components/Header'
 import { Socials } from '../../components/Socials'
+import profile from '../../assets/profile-img.png'
 
 export function Home() {
   return (
     <Container>
       <Header />
       <Socials />
+      <main>
+        <div className="title-content">
+          <h1>
+            renan <br /> silva
+          </h1>
+          <h2>
+            desenvolvedor <br /> frontend
+          </h2>
+        </div>
+        <div className="img-content">
+          <img src={profile} alt="Image de Renan Silva" />
+        </div>
+      </main>
     </Container>
   )
 }
--- portfolio/src/pages/Home/style.js
+++ portfolio/src/pages/Home/style.js
@@ -7,4 +7,33 @@ export const Container = styled.div`
   background-position: center top;
   background-repeat: no-repeat;
   background-size: cover;
+
+  main {
+    max-width: 1120px;
+    margin: 40px auto;
+    padding: 0 62px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 20px;
+
+    .title-content {
+      font-family: ${({ theme }) => theme.FONTS.FONT_IBM};
+      text-transform: uppercase;
+      color: white;
+      text-align: center;
+
+      h1 {
+        font-size: 118px;
+        line-height: 91px;
+        margin-bottom: 38px;
+        text-shadow: 7px 7px 5px rgba(0, 0, 0, 0.7);
+      }
+      h2 {
+        font-size: 42px;
+        line-height: 42px;
+        text-shadow: 7px 7px 5px rgba(0, 0, 0, 0.7);
+      }
+    }
+  }
 `
