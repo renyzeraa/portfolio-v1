1e9638e43423638761c0363be476798868ddbea0
Renan Silva <renansilvaytB@gmail.com>
2022-11-12 14:15:48 -0300

    feat projects slide

--- portfolio/src/pages/Projects/index.jsx
+++ portfolio/src/pages/Projects/index.jsx
@@ -3,13 +3,66 @@ import { Header } from '../../components/Header'
 import { Socials } from '../../components/Socials'
 import { Title } from '../../components/Title'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Pagination, Navigation } from 'swiper'
+
+import beauty from '../../assets/projects/beauty.png'
+import gpm from '../../assets/projects/gpm.png'
+import doctorcare from '../../assets/projects/doctorcare.png'
+import mymovies from '../../assets/projects/mymovies.png'
+
 export function Projects() {
   return (
     <Container>
       <Header />
       <Socials />
-      <main>
+      <main className="wrapper">
         <Title>Projetos</Title>
+        <div className="slide-content">
+          <Swiper
+            spaceBetween={20}
+            className="mySwiper"
+            // loopFillGroupWithBlank={true}
+            pagination={{
+              clickable: true
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+          >
+            <SwiperSlide className="card">
+              <img src={beauty} alt="" />
+              <h2>Beauty Salon</h2>
+              <p>
+                Landing page, apresentando salão de beleza, depoimentos e
+                contato.
+              </p>
+            </SwiperSlide>
+            <SwiperSlide className="card">
+              <img src={gpm} alt="" />
+              <h2>GPM Car</h2>
+              <p>
+                Landing page, empresa seguimento automotivo, como todos os
+                serviços que a empresa oferece.
+              </p>
+            </SwiperSlide>
+            <SwiperSlide className="card">
+              <img src={doctorcare} alt="" />
+              <h2>Doctor Care</h2>
+              <p>Landing page, página para apresentar clinica médica.</p>
+            </SwiperSlide>
+            <SwiperSlide className="card">
+              <img src={mymovies} alt="" />
+              <h2>My Movies</h2>
+              <p>
+                Aplicação para salvar seus filmes favoritos, consumindo APIRest
+                própria.
+              </p>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </main>
     </Container>
   )
--- portfolio/src/pages/Projects/style.js
+++ portfolio/src/pages/Projects/style.js
@@ -9,6 +9,43 @@ export const Container = styled.div`
   background-size: cover;
   height: 100vh;
 
+  .slide-content {
+    padding-top: 60px;
+    max-width: 1100px;
+    margin: 0 auto;
+  }
+
+  .mySwiper {
+    padding: 0 48px;
+  }
+
+  .card {
+    background-color: ${({ theme }) => theme.COLORS.GRAY_100};
+    height: 400px;
+    max-width: 280px;
+    padding: 20px;
+    border-radius: 10px;
+    font-family: ${({ theme }) => theme.FONTS.FONT_FIRA_SANS};
+    img {
+      width: 100%;
+      height: 50%;
+      display: block;
+      object-position: center left;
+      object-fit: cover;
+      border-radius: 5px;
+      margin-bottom: 32px;
+    }
+
+    h2 {
+      text-align: center;
+      margin-bottom: 32px;
+    }
+
+    p {
+      text-align: center;
+    }
+  }
+
   @media (max-width: 800px) {
     background-image: url(${bgmobile});
   }
