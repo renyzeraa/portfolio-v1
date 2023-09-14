feb17a01a49bd2d579c686e834b574b29e24034c
Renan Silva <renansilvaytB@gmail.com>
2023-09-14 19:02:28 -0300

    feat about page

--- portfolio/src/pages/About/index.jsx
+++ portfolio/src/pages/About/index.jsx
@@ -2,6 +2,7 @@ import { Container } from './style'
 import { Header } from '../../components/Header'
 import { Socials } from '../../components/Socials'
 import { Footer } from '../../components/Footer'
+import { Title } from '../../components/Title'
 
 export function About() {
   return (
@@ -9,6 +10,10 @@ export function About() {
       <Socials />
       <Header />
       <main className='main-container'>
+        <Title>Sobre</Title>
+        <div className='container'>
+          
+        </div>
       </main>
       <Footer />
     </Container>
--- portfolio/src/pages/About/style.js
+++ portfolio/src/pages/About/style.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components'
+import bgAbout from '../../assets/bg/bg-about.png'
+import bgAboutMobile from '../../assets/bg/bg-about-mob.png'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  height: 100vh;
+  background-image: url(${bgAboutMobile});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  
+`
