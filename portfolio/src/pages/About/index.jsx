d9a21ca3a8d5c1aa196a2dc28b1fb9e076a48f5d
Renan Silva <renansilvaytB@gmail.com>
2023-09-19 17:46:53 -0300

    feat about page

--- portfolio/src/pages/About/index.jsx
+++ portfolio/src/pages/About/index.jsx
@@ -1,8 +1,19 @@
-import { Container } from './style'
+import { Container, Text } from './style'
 import { Header } from '../../components/Header'
 import { Socials } from '../../components/Socials'
 import { Footer } from '../../components/Footer'
 import { Title } from '../../components/Title'
+import css from '../../assets/techs/css.svg'
+import git from '../../assets/techs/git.svg'
+import html from '../../assets/techs/html.svg'
+import js from '../../assets/techs/js.svg'
+import react from '../../assets/techs/react.svg'
+import typescript from '../../assets/techs/typescript.svg'
+import jquery from '../../assets/techs/jquery.svg'
+import sass from '../../assets/techs/sass.svg'
+import postgresql from '../../assets/techs/postgre.svg'
+import php from '../../assets/techs/php.svg'
+import node from '../../assets/techs/node.svg'
 
 export function About() {
   return (
@@ -12,7 +23,26 @@ export function About() {
       <main className='main-container'>
         <Title>Sobre</Title>
         <div className='container'>
-          
+          <div className="text-area">
+            <Text>Prazer sou Renan Leandro da Silva 👋🏼, mas pode me chamar de renyzera também.</Text>
+            <Text>Desenvolvo projetos utilizando React.js, styled-components ou SASS com TypeScript, que vão desde uma Landing Page até aplicações consumindo API’s. Algumas aplicações web que fiz, você pode conferir na aba <a href=""></a> e também la no meu github.</Text>
+            <Text>Pretendo seguir minha carreira como DEV FrontEnd, sempre resolvendo problemas com códigos, seguindo metodologia ágil e que tenham um bom desempenho e entregando valor para o cliente final.</Text>
+          </div>
+          <div className='technologies-container'>
+            <ul className='techs-list'>
+              <li><img src={css} alt="Linguagem de Programação"/></li>
+              <li><img src={html} alt="Linguagem de Programação"/></li>
+              <li><img src={js} alt="Linguagem de Programação"/></li>
+              <li><img src={git} alt="Linguagem de Programação"/></li>
+              <li><img src={react} alt="Linguagem de Programação"/></li>
+              <li><img src={jquery} alt="Linguagem de Programação"/></li>
+              <li><img src={sass} alt="Linguagem de Programação"/></li>
+              <li><img src={typescript} alt="Linguagem de Programação"/></li>
+              <li><img src={node} alt="Linguagem de Programação"/></li>
+              <li><img src={php} alt="Linguagem de Programação"/></li>
+              <li><img src={postgresql} alt="Linguagem de Programação"/></li>
+            </ul>
+          </div>
         </div>
       </main>
       <Footer />
--- portfolio/src/pages/About/style.js
+++ portfolio/src/pages/About/style.js
@@ -8,6 +8,38 @@ export const Container = styled.div`
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
+  overflow: auto;
 
-  
+  .main-container {
+    .container {
+      .text-area {
+
+      }
+
+      .technologies-container {
+        width: 100%;
+
+        .techs-list {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          justify-content: space-evenly;
+          gap: 20px;
+
+          li {
+            flex: 0 0 60px;
+          }
+        }
+      }
+    }
+  }
+`
+
+export const Text = styled.p`
+  color:  ${({ theme }) => theme.COLORS.GRAY_100};
+  text-align: center;
+  font-family: ${({ theme }) => theme.FONTS.FONT_FIRA_SANS};
+  font-size: 22px;
+  line-height: normal;
+  margin-bottom: 1.4rem;
 `
