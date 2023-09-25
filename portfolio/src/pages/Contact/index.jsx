bd2153be91f714e8178c976317ca459507385801
Renan Silva <renansilvaytB@gmail.com>
2023-09-25 19:14:02 -0300

    feat contact page

--- portfolio/src/pages/Contact/index.jsx
+++ portfolio/src/pages/Contact/index.jsx
@@ -1,14 +1,116 @@
-import { Container } from './style'
+import { Container, Campo, Button } from './style'
+import { useState, useEffect } from 'react'
 import { Header } from '../../components/Header'
 import { Socials } from '../../components/Socials'
 import { Footer } from '../../components/Footer'
+import { Title } from '../../components/Title'
+import axios from 'axios'
 
 export function Contact() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000); // Atualizar a cada segundo
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const formattedDate = currentDate.toLocaleString();
+
+  const code = `
+    const button = document.querySelector('#sendBtn'); 
+    
+    const message = { 
+      name: "${nome}", 
+      email: "${email}", 
+      message: "${mensagem}", 
+      date: "${formattedDate}" 
+    } 
+    
+    button.addEventListener('click', () => { 
+      form.send(message); 
+    });`;
+
+  const lines = code.split('\n');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/enviar-email', {
+        nome,
+        email,
+        mensagem,
+      });
+
+      console.log('Email enviado com sucesso!', response.data);
+    } catch (error) {
+      console.error('Erro ao enviar o email', error);
+    }
+  };
+
   return (
     <Container>
       <Socials />
       <Header />
       <main className='main-container'>
+        <Title>Sobre</Title>
+        <div className='container'>
+          <form className='form-container' onSubmit={handleSubmit}>
+            <Campo>
+              <label>_nome:</label>
+              <input
+                type="text"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                required
+              />
+            </Campo>
+            <Campo>
+              <label>_email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Campo>
+            <Campo>
+              <label>_mensagem:</label>
+              <textarea
+                value={mensagem}
+                onChange={(e) => setMensagem(e.target.value)}
+                rows="6"
+                required
+              />
+            </Campo>
+            <Button type="submit">Enviar</Button>
+          </form>
+            <div className="code-card">
+              <div className="line-numbers">
+                {lines.map((_, index) => (
+                  <div key={index + 1} className="line-number">
+                    {index + 1}
+                  </div>
+                ))}
+              </div>
+              <div className="code-content">
+                {lines.map((line, index) => (
+                  <div key={index} className="code-line">
+                    {line}
+                  </div>
+                ))}
+              </div>
+            </div>
+        </div>
       </main>
       <Footer />
     </Container>
--- portfolio/src/pages/Contact/style.js
+++ portfolio/src/pages/Contact/style.js
@@ -10,7 +10,137 @@ export const Container = styled.div`
   background-size: cover;
   overflow: auto;
 
+  .main-container {
+    padding: 0 56px;
+
+    .form-container {
+      margin-bottom: 48px;
+    }
+
+    .code-card {
+      display: none;
+    }
+  }
+
+
   @media (min-width: 728px){
     background-image: url(${bgContact});
+
+    .main-container {
+      padding: 0 48px;
+      
+      .code-card {
+        margin-top: 24px;
+        flex: 1;
+        display: flex;
+        font-family: ${({ theme }) => theme.FONTS.FONT_FIRA_CODE};
+        height: 425px;
+        max-height: 100%;
+
+        .line-numbers {
+          flex: 0 0 40px;
+          background-color: #333;
+          color: #ccc;
+          padding: 10px;
+          text-align: right;
+          border-right: 1px solid #555;
+          height: 100%;
+          border-radius: 8px 0px 0px 8px;
+          
+          .line-number {
+            margin-bottom: 5px;
+          }
+        }
+
+        .code-content {
+          flex: 1;
+          padding: 10px;
+          background-color: #f5f5f5;
+          width: 100%;
+          height: 100%;
+          border-radius: 0px 8px 8px 0px;
+          .code-line {
+            white-space: pre-wrap;
+            word-wrap: break-word;
+          }
+        }
+      }
+      .form-container {
+        flex: 1;
+      }
+    }
   }
-  `
+
+  @media (min-width: 820px){
+    .main-container {
+      .container {
+        display: flex;
+        gap: 12px;
+        height: 100%;
+      }
+    }
+  }
+
+  @media (min-width: 1200px){
+    .main-container {
+      padding: 0 24px;
+    }
+  }
+`
+
+export const Campo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-bottom: 1rem;
+
+  label {
+    color: var(--c1, #F5F5F5);
+    font-family: Fira Code;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 450;
+    line-height: normal;
+  }
+
+  input, textarea {
+    border-radius: 8px;
+    border: 1px solid var(--c12, #292929);
+    background: var(--c12, #292929);
+    height: 2.5rem;
+    padding: 8px;
+    font-size: 18px;
+    color: var(--c1, #F5F5F5);
+  }
+
+  textarea {
+    height: 100%;
+    resize: none;
+  }
+`
+export const Button = styled.button`
+  border: none;
+  border-radius: 8px;
+  background: var(#F0F0F0);
+  display: flex;
+  width: 185px;
+  height: 48px;
+  padding: 10px 14px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  color: var(#292929);
+  text-align: right;
+  font-family: Fira Code;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: normal;
+
+  &:hover {
+    background: #292929;
+    color: #F0F0F0;
+
+    transition: background 0.8s ease-out;
+  }
+`
